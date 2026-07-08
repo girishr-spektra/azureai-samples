9762f9822e1c984394c04aa7869b4736bedf00e0
Girish R <ted.girish17@gmail.com>
2026-07-08 12:13:00 +0530

    up

--- scenarios/model-catalog/README.md
+++
@@ -1,2 +0,0 @@
-# Model catalog
-Contains the getting started samples for model catalog.
--- scenarios/projects/basic/project.py
+++
@@ -1,8 +0,0 @@
-from azure.identity import DefaultAzureCredential
-from azure.ai.projects import AIProjectClient
-
-project_connection_string = "your_connection_string"
-
-project = AIProjectClient.from_connection_string(
-    conn_str=project_connection_string, credential=DefaultAzureCredential()
-)
