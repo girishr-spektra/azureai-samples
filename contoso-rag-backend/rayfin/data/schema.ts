2aac26def5f4a26cd673ddc4c989cda54962f53a
Girish R <ted.girish17@gmail.com>
2026-07-08 12:11:47 +0530

    up

--- contoso-rag-backend/rayfin/data/schema.ts
+++
@@ -1,9 +0,0 @@
-import { Todo } from './Todo.js';
-import { ChatInteraction } from './ChatInteraction.js';
-
-export type TodoAppSchema = {
-  Todo: Todo;
-  ChatInteraction: ChatInteraction;
-};
-
-export const schema = [Todo, ChatInteraction];
